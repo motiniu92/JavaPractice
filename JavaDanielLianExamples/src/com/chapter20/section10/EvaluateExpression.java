9b3a7362a574c11b158fb7a085168711ad4a64fc
motiniu92 <motiniu92@gmail.com>
2020-09-15 08:27:07 +0600

    Evaluate Expression

---
+++ JavaDanielLianExamples/src/com/chapter20/section10/EvaluateExpression.java
@@ -0,0 +1,133 @@
+package com.chapter20.section10;
+
+import java.util.Stack;
+
+public class EvaluateExpression {
+    public static void main(String[] args) {
+        // Chec number of arguments passed
+        if (args.length != 1) {
+            System.out.println("usage : java Evaluate Expression \"expression\"");
+            System.exit(1);
+        }
+
+        try {
+            System.out.println(evaluateExpression(args[0]));
+        } catch (Exception ex) {
+            System.out.println("Wrong expression : " + args[0]);
+        }
+    }
+
+    // Evaluate and expression
+    public static int evaluateExpression(String expression) {
+        // Create operandStack to store operands
+        Stack<Integer> operandStack = new Stack<>();
+
+        // Create operatorStack to store operator
+        Stack<Character> operatorStack = new Stack<>();
+
+        //Insert blanks around (,), +, -, /, and *
+        expression = insertBlanks(expression);
+
+        // Extract operands and operators
+        String[] tokens = expression.split(" ");
+
+        // Phase 1: Scan tockens
+        for (String token : tokens) {
+            // Blank space
+            if (token.length() == 0) {
+                continue; // Back to the while loop to extract the next token
+            } else if (token.charAt(0) == '+' || token.charAt(0) == '-') {
+                //Process all +, -, *, / in the top of the operator stack
+                while (!operatorStack.isEmpty() &&
+                        (operatorStack.peek() == '+' ||
+                                operatorStack.peek() == '-' ||
+                                operatorStack.peek() == '*' ||
+                                operatorStack.peek() == '/')) {
+                    processAnOperator(operandStack, operatorStack);
+
+                }
+
+                // Push the + or - operator into the operator stack
+                operatorStack.push(token.charAt(0));
+            } else if (token.charAt(0) == '*' || token.charAt(0) == '/') {
+                // Process all * , / in the top of the operator stack
+                while (!operatorStack.isEmpty() &&
+                        (operatorStack.peek() == '*' ||
+                                operatorStack.peek() == '/')) {
+                    processAnOperator(operandStack, operatorStack);
+                }
+
+                // Push the * or / operator into the operator stack
+                operatorStack.push(token.charAt(0));
+
+            } else if (token.trim().charAt(0) == '(') {
+                operatorStack.push('('); // Push '(' to stack
+
+            } else if (token.trim().charAt(0) == ')') {
+
+                // Process all the operators in the stack until seeing '('
+                while (operatorStack.peek() != '(') {
+                    processAnOperator(operandStack, operatorStack);
+                }
+
+                operatorStack.pop(); // Pop the '(' symbol from the stack
+
+            } else { // An operand scanned
+                // Push an operand to the stack
+                operandStack.push(new Integer(token));
+            }
+        }
+
+        // Phase 2: Process all the remaining operators in the stack
+        while (!operatorStack.isEmpty()) {
+            processAnOperator(operandStack, operatorStack);
+        }
+
+        // Return the result
+        return operandStack.pop();
+    }
+
+
+
+    /* Process one operator: Take an operator from operatorStack and
+         apply it on the operands in the operandStack */
+    public static void processAnOperator(
+            Stack<Integer> operandStack, Stack<Character> operatorStack) {
+        char op = operatorStack.pop();
+        int op1 = operandStack.pop();
+        int op2 = operandStack.pop();
+        if (op == '+') {
+            operandStack.push(op2 + op1);
+
+        } else if (op == '-') {
+            operandStack.push(op2 - op1);
+
+        } else if (op == '*') {
+            operandStack.push(op2 * op1);
+
+        } else if (op == '/') {
+            operandStack.push(op2 / op1);
+        }
+
+    }
+
+
+
+    private static String insertBlanks(String s) {
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == ')' ||
+                    s.charAt(i) == '+' || s.charAt(i) == '-' ||
+                    s.charAt(i) == '*' || s.charAt(i) == '/') {
+                result += " " + s.charAt(i) + " ";
+
+            } else {
+                result += s.charAt(i);
+            }
+        }
+        return result;
+    }
+
+
+}
