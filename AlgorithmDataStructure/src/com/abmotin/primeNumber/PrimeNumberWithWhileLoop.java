4412ac9ca90ba1b3716b68618a5821547b3b0a55
motiniu92 <motiniu92@gmail.com>
2024-04-24 16:17:21 +0600

    primeNumber with while loop

--- AlgorithmDataStructure/src/com/abmotin/primeNumber/PrimeNumberWithWhileLoop.java
+++ AlgorithmDataStructure/src/com/abmotin/primeNumber/PrimeNumberWithWhileLoop.java
@@ -1,4 +1,28 @@
 package com.abmotin.primeNumber;
 
+import java.util.Scanner;
+
 public class PrimeNumberWithWhileLoop {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int num = scan.nextInt();
+        int i = 2;
+        boolean isPrime = false;
+
+        while (i <= num / 2) {
+            if (num % i == 0) {
+                isPrime = true;
+                break;
+            }
+            ++i;
+        }
+
+        if (!isPrime) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number");
+        }
+    }
 }
