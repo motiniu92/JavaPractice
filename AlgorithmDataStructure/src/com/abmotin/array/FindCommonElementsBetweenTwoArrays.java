04ce7f6c43d09efd7d2e624b86bf0c84db704d18
motiniu92 <motiniu92@gmail.com>
2024-06-03 14:37:24 +0600

    Find common element in two arrays

---
+++ AlgorithmDataStructure/src/com/abmotin/array/FindCommonElementsBetweenTwoArrays.java
@@ -0,0 +1,28 @@
+package com.abmotin.array;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class FindCommonElementsBetweenTwoArrays {
+    public static void main(String[] args) {
+
+        String[] array1 = {"Android", "JAVA", "PHP", "C#", "C++", "SQL"};
+        String[] array2 = {"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"};
+
+        System.out.println("Array1 : " + Arrays.toString(array1));
+        System.out.println("Array2 : " + Arrays.toString(array2));
+
+        HashSet set = new HashSet();
+
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i].equals(array2[j])) {
+
+                    set.add(array1[i]);
+                }
+            }
+        }
+
+        System.out.println("Common element : " + (set)); // OUTPUT: [SQL, JAVA]
+    }
+}
